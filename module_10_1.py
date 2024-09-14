1da94a2d019df8215b5915df97ecb14a237d1f7a
0tr1cala-v <marenkov2015@mail.ru>
2024-09-14 12:01:06 +0300

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,41 @@
+from time import sleep
+from threading import Thread
+from datetime import datetime
+import requests
+
+def wite_words(word_count, file_name):
+    with open(file_name, 'a', encoding='utf-8') as file:
+        for i in range(word_count):
+            file.write(f'Какое-то слово № {i}\n')
+            sleep(0.1)
+    print(f'Завершилась запись в файл {file_name}')
+
+
+start_time1 = datetime.now()
+wite_words(10, 'example1.txt')
+wite_words(30, 'example2.txt')
+wite_words(200, 'example3.txt')
+wite_words(100, 'example4.txt')
+stop_time1 = datetime.now()
+res_time1 = stop_time1 - start_time1
+print(f'Работа функций: {res_time1}')
+
+start_time2 = datetime.now()
+thr_first = Thread(target=wite_words, args=(10, 'example5.txt'))
+thr_second = Thread(target=wite_words, args=(30, 'example6.txt'))
+thr_third = Thread(target=wite_words, args=(200, 'example7.txt'))
+thr_fourth = Thread(target=wite_words, args=(100, 'example8.txt'))
+
+thr_first.start()
+thr_second.start()
+thr_third.start()
+thr_fourth.start()
+
+thr_first.join()
+thr_second.join()
+thr_third.join()
+thr_fourth.join()
+
+stop_time2 = datetime.now()
+res_time2 = stop_time2 - start_time2
+print(f'Работа потоков: {res_time2}')
